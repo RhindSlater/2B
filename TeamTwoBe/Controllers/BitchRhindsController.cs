d72732dad01f79538c6b2f96cda829b0b8e113f0
jtd2017 <joseph4343@hotmail.com>
2019-02-21 20:24:09 +1300

    asasdfasdfasdf

---
+++ TeamTwoBe/Controllers/BitchRhindsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamTwoBe.Controllers
+{
+    public class BitchRhindsController : Controller
+    {
+        // GET: BitchRhinds
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
