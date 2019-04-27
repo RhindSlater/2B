61415cbddbe19870bd9b088e200e1a5759690c25
Sahil Puri <2021101@techtorium.ac.nz>
2019-04-27 15:13:32 +1200

    Added Auction Controller and Viewmodel

---
+++ TeamTwoBe/Controllers/AuctionController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamTwoBe.ViewModels;
+
+namespace TeamTwoBe.Controllers
+{
+    public class AuctionController : Controller
+    {
+        // GET: Auction
+        public ActionResult Index()
+        {
+            SaleUserCardVM auctionVM = new SaleUserCardVM { };
+            return View();
+        }
+
+        // GET: Auction/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Auction/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auction/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auction/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Auction/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auction/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Auction/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
