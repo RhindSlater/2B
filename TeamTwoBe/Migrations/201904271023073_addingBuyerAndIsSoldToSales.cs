da8b21ce52fda1494c9219755c1e85b90dadaa9f
RhindSlater <rhindslater@gmail.com>
2019-05-02 17:14:42 +1200

    Update 201904271023073_addingBuyerAndIsSoldToSales.cs

--- TeamTwoBe/Migrations/201904271023073_addingBuyerAndIsSoldToSales.cs
+++ TeamTwoBe/Migrations/201904271023073_addingBuyerAndIsSoldToSales.cs
@@ -10,14 +10,20 @@ namespace TeamTwoBe.Migrations
             DropForeignKey("dbo.Sales", "Seller_ID", "dbo.Users");
             AddColumn("dbo.Sales", "IsSold", c => c.Boolean(nullable: false));
             AddColumn("dbo.Sales", "Buyer_ID", c => c.Int());
+            AddColumn("dbo.Sales", "User_ID", c => c.Int());
             CreateIndex("dbo.Sales", "Buyer_ID");
+            CreateIndex("dbo.Sales", "User_ID");
             AddForeignKey("dbo.Sales", "Buyer_ID", "dbo.Users", "ID");
+            AddForeignKey("dbo.Sales", "User_ID", "dbo.Users", "ID");
         }
-        
+
         public override void Down()
         {
+            DropForeignKey("dbo.Sales", "User_ID", "dbo.Users");
             DropForeignKey("dbo.Sales", "Buyer_ID", "dbo.Users");
+            DropIndex("dbo.Sales", new[] { "User_ID" });
             DropIndex("dbo.Sales", new[] { "Buyer_ID" });
+            DropColumn("dbo.Sales", "User_ID");
             DropColumn("dbo.Sales", "Buyer_ID");
             DropColumn("dbo.Sales", "IsSold");
             AddForeignKey("dbo.Sales", "Seller_ID", "dbo.Users", "ID", cascadeDelete: true);
