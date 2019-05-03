c02c96014bf469a330e8038779c83b71a530e770
RhindSlater <rhindslater@gmail.com>
2019-05-03 22:56:40 +1200

    updating format

--- TeamTwoBe/Migrations/201902230922329_DoStuffPlease1.cs
+++ TeamTwoBe/Migrations/201902230922329_DoStuffPlease1.cs
@@ -2,7 +2,7 @@ namespace TeamTwoBe.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class DoStuffPlease1 : DbMigration
     {
         public override void Up()
@@ -10,31 +10,31 @@ namespace TeamTwoBe.Migrations
             CreateTable(
                 "dbo.Followers",
                 c => new
-                    {
-                        User_ID = c.Int(nullable: false),
-                        User_ID1 = c.Int(nullable: false),
-                    })
+                {
+                    User_ID = c.Int(nullable: false),
+                    User_ID1 = c.Int(nullable: false),
+                })
                 .PrimaryKey(t => new { t.User_ID, t.User_ID1 })
                 .ForeignKey("dbo.Users", t => t.User_ID)
                 .ForeignKey("dbo.Users", t => t.User_ID1)
                 .Index(t => t.User_ID)
                 .Index(t => t.User_ID1);
-            
+
             CreateTable(
                 "dbo.Wishlist",
                 c => new
-                    {
-                        Card_ID = c.Int(nullable: false),
-                        User_ID = c.Int(nullable: false),
-                    })
+                {
+                    Card_ID = c.Int(nullable: false),
+                    User_ID = c.Int(nullable: false),
+                })
                 .PrimaryKey(t => new { t.Card_ID, t.User_ID })
                 .ForeignKey("dbo.Cards", t => t.Card_ID, cascadeDelete: true)
                 .ForeignKey("dbo.Users", t => t.User_ID, cascadeDelete: true)
                 .Index(t => t.Card_ID)
                 .Index(t => t.User_ID);
-            
+
         }
-        
+
         public override void Down()
         {
             DropForeignKey("dbo.Wishlist", "User_ID", "dbo.Users");
