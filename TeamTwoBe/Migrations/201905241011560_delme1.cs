dca418baa454272fbecb5c6feff636a79e83eadb
RhindSlater <rhindslater@gmail.com>
2019-05-25 15:42:26 +1200

    Styling Change

---
+++ TeamTwoBe/Migrations/201905241011560_delme1.cs
@@ -0,0 +1,16 @@
+namespace TeamTwoBe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class delme1 : DbMigration
+    {
+        public override void Up()
+        {            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
